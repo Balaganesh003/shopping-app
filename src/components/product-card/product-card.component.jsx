0ee17cd083b1726b9b18b2a3a204140e2a11234f
Balaganesh003 <k.balaganesh26@gmail.com>
2022-10-01 11:39:25 +0530

    Adding cart context , shopping icon and cart dropdown

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -3,7 +3,7 @@ import './product-card.styles.scss';
 import Button from '../button/button.component';
 
 const ProductCard = ({ product }) => {
-    const { name, price, imageUrl } = product;
+    const { name, price, imageUrl } = product;  
     return (
         <div className='product-card-container'>
             <img src={imageUrl} alt={`${name}`} />
@@ -11,9 +11,10 @@ const ProductCard = ({ product }) => {
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
+
             <Button buttonType='inverted'>Add to cart</Button>
         </div>
-    );
+    ); 
 };
 
 export default ProductCard;
