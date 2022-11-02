3c9b5692223492a8013dbda17912d82703f4959c
Balaganesh003 <k.balaganesh26@gmail.com>
2022-11-02 17:19:50 +0530

    Added the loading spinner for category and categries-preview using redux-Thunk

--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,37 @@
-import { compose, createStore, applyMiddleware } from "redux";
+import { compose, createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import thunk from 'redux-thunk';
 
-import { rootReducer } from "./root-reducer";
+import { loggerMiddleware } from './middleware/logger';
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-  console.log("type: ", action.type);
-  console.log("payload: ", action.payload);
-  console.log("currentState: ", store.getState());
+import { rootReducer } from './root-reducer';
 
-  next(action);
-  console.log("nextState: ", store.getState());
+const middleWares = [
+  process.env.NODE_ENV === 'development' && loggerMiddleware,
+  thunk,
+].filter(Boolean);
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
 };
 
-const middleWares = [loggerMiddleware];
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
